614825d807e1876b209cb4953918ab1415796ccf
Swayam-Dhungana <swayamdhungana.official@gmail.com>
2025-06-23 21:39:03 +0530

    Updated

--- backend/dist/src/lib/firebase.js
+++ backend/dist/src/lib/firebase.js
@@ -5,11 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.adminFirestore = exports.adminAuth = void 0;
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
-const serviceAccountKey_json_1 = __importDefault(require("../../serviceAccountKey.json")); //Connect a service account firebase
-//This is required for user Validation in the backend
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
 if (!firebase_admin_1.default.apps.length) {
     firebase_admin_1.default.initializeApp({
-        credential: firebase_admin_1.default.credential.cert(serviceAccountKey_json_1.default),
+        credential: firebase_admin_1.default.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey,
+        }),
     });
 }
 exports.adminAuth = firebase_admin_1.default.auth();
