2074b612bd175a34cbec472f6f09a4e5c705914f
Swayam-Dhungana <swayamdhungana.official@gmail.com>
2025-06-23 23:03:40 +0530

    Checking

--- backend/index.ts
+++ backend/index.ts
@@ -5,23 +5,24 @@ import authRoutes from './src/routes/api'
 import hotelRoutes from './src/routes/hotel'
 
 const app = new Hono()
+
 app.use('*', async (c, next) => {
-  const origin = process.env.BASEURL as string
+  const origin = process.env.BASEURL || 'https://toura-swart.vercel.app'
+
   c.header('Access-Control-Allow-Origin', origin)
   c.header('Access-Control-Allow-Credentials', 'true')
   c.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   c.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
 
   if (c.req.method === 'OPTIONS') {
-    return c.text('', 400) 
+    return c.text('', 200) 
   }
 
   await next()
 })
-//Comment for last Deployment
+
 app.route('/api', authRoutes)
-app.route('/api/v1',hotelRoutes)
+app.route('/api/v1', hotelRoutes)
 app.get('/', (c) => c.text('Hello from Hono + Node.js!'))
 
 serve({ fetch: app.fetch, port: 3000 })
-
